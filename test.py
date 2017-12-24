79a17a070f32748400415b5684287da673041e1f
tacchan7412 <tacchan.04@gmail.com>
2017-12-24 22:27:00 +0900

    fix test.py to func

--- test.py
+++ test.py
@@ -11,23 +11,31 @@ opt.nThreads = 1   # test code only supports nThreads = 1
 opt.batchSize = 1  # test code only supports batchSize = 1
 opt.serial_batches = True  # no shuffle
 opt.no_flip = True  # no flip
+opt.gpu_ids = -1
+opt.name = 'h2g_cyclegan'
+opt.dataset_mode = 'single'
+opt.model = 'test'
+opt.phase = 'test'
+
 
 data_loader = CreateDataLoader(opt)
-dataset = data_loader.load_data()
 model = create_model(opt)
 visualizer = Visualizer(opt)
 # create website
 web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
 webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
-# test
-for i, data in enumerate(dataset):
-    if i >= opt.how_many:
-        break
-    model.set_input(data)
-    model.test()
-    visuals = model.get_current_visuals()
-    img_path = model.get_image_paths()
-    print('%04d: process image... %s' % (i, img_path))
-    visualizer.save_images(webpage, visuals, img_path)
 
-webpage.save()
+def test():
+    dataset = data_loader.load_data()
+    # test
+    for i, data in enumerate(dataset):
+        if i >= opt.how_many:
+            break
+        model.set_input(data)
+        model.test()
+        visuals = model.get_current_visuals()
+        img_path = model.get_image_paths()
+        print('%04d: process image... %s' % (i, img_path))
+        visualizer.save_images(webpage, visuals, img_path)
+
+    webpage.save()
